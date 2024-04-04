8345f5c12352bd3e67bb47bda0b8a9afeee2ab66
unknown <amazighmalek.com>
2024-04-04 18:00:45 +0200

    all code

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+
+function App() {
+  return (
+    <>
+      <h1 className='text-center my-5 text-3xl font-medium'>
+        Vite Tailwind css lib UI</h1>
+
+       
+    </>
+  )
+}
+
+export default App
